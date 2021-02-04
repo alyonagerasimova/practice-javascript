b88bf8c996afde82dc6e8a798dadf20065cf4ab3
Alyona <alyonagerasimova@gmail.com>
2021-02-04 19:21:26 +0400

    first commit

---
+++ run.js
@@ -0,0 +1,14 @@
+"use sctrict";
+
+// const result = confirm("Are you here?");
+
+// const answer = prompt("Dfv tcnm 18?", "");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// console.log(typeof(answers));
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
